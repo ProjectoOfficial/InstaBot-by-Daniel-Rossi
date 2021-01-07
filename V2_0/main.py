eec4cc014a874d56596143a7863ddd61a7dea51f
ProjectoOfficial <miniprojectsofficial@gmail.com>
2021-01-07 18:14:58 +0100

    Update main.py

--- V2_0/main.py
+++ V2_0/main.py
@@ -1,8 +1,8 @@
 from instabot.V2_0 import bot
 
 #parametri da impostare
-username = 'danielrossitv'
-password = 'LoveBasket18!'
+username = ''
+password = ''
 
 if __name__ == '__main__':
     bot = bot.Bot(username, password)
